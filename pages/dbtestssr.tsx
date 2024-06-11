4b38aa6b46a18b853d8540ad4777664a50674fb7
BuildSlave <jfmsteam2@gmail.com>
2024-06-12 00:06:57 +0200

    fix env

--- pages/dbtestssr.tsx
+++ pages/dbtestssr.tsx
@@ -1,27 +1,30 @@
 import Image from "next/image";
 import { useState, useEffect, use } from 'react';
 
+import { usePathname } from "next/navigation";
 
+import { headers } from 'next/headers';
 
 
 
 
 export async function getServerSideProps(){
-  const url = process.env.URL;
+  const url = process.env.HOST;
+  console.log(url);
   const response = await fetch(url + 'api/test');
   const data = await response.json();
   console.log(data);
   return {
     props: {
       data : JSON.parse(JSON.stringify(data)),
+      path: url
     },
   };
 }
 
 
 
-export default function Home({ data }: { data: any }) {
-
+export default function Home({ data }: { data: any }, {path}: {path: any}) {
   const [datay, setData] = useState(null);
   const [newd, setNew] = useState(false);
   async function fetchData() {
@@ -38,6 +41,7 @@ export default function Home({ data }: { data: any }) {
         <h1>DATA</h1>
         <pre>{JSON.stringify(data, null, 2)}</pre>
         <button onClick={fetchData}>Fetch Data</button>
+        {path}
       </div>
     );
   }else{
@@ -46,6 +50,7 @@ export default function Home({ data }: { data: any }) {
         <h1>DATA</h1>
         <pre>{JSON.stringify(datay, null, 2)}</pre>
         <button onClick={fetchData}>Fetch Data</button>
+        {path}
       </div>
     );
   }
